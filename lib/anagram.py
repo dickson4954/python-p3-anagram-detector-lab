7049d7df92be21d896e16e9bbd494b6f26fb6788
dickson4954 <dicksonmurithi155@gmail.com>
2024-05-29 19:36:22 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,8 @@
-# your code goes here!
+class Anagram:
+    def __init__(self,word):
+       self.word = word
+    def match(self, text):
+        return [word for word in text if sorted(word) == sorted(self.word)]
+        
+    pass
+    
